b034e3895331f87837561e3cd0c5802ea9462eaf
Laxmi Thoodi <laxmithoodi@zipcoders-mbp-3.lan>
2019-02-05 22:02:02 -0500

    guessinggame lab

--- Main.java
+++ Main.java
@@ -5,10 +5,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Random;
+import java.util.Scanner;
 public class Main
 {
     // instance variables - replace the example below with your own
-    private int x;
+    //private int x;
 
     /**
      * Constructor for objects of class Main
@@ -16,7 +18,9 @@ public class Main
     public Main()
     {
         // initialise instance variables
-        x = 0;
+        //x = 0;
+         guessnumb();
+        
     }
 
     /**
@@ -25,9 +29,44 @@ public class Main
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public void guessnumb()
     {
         // put your code here
-        return x + y;
+        final int num =10;
+        int numberToGuess; 
+        int guess;
+        int count =0;
+        boolean result = false;
+        
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        numberToGuess = rand.nextInt(num)+1;
+        
+        while(result == false)
+        {
+            System.out.println( "Enter Guess number: " );
+            guess = input.nextInt();
+            if(guess>numberToGuess)
+            {
+                System.out.println("Your guess is too large");
+            }
+            else if(guess<numberToGuess)
+            {
+                System.out.println("Your guess is too small");
+            }
+            else
+            {
+                System.out.println("Your guess is correct");
+                
+                result=true;
+                
+            }
+            count++;
+        
+        }
+        
+        System.out.println("Number of tries" +count); 
+        //return numberToGuess;
+        //return x + y;
     }
 }
